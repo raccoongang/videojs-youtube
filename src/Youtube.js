b870010537e02253ea9f5750132f293fbce98078
dorosh <dorosshh@gmail.com>
2017-02-17 13:04:18 +0200

    Fix volume state (#1)
    - add attr volumeInfo for save volume state

--- src/Youtube.js
+++ src/Youtube.js
@@ -521,7 +521,7 @@ THE SOFTWARE. */
     },
 
     volume: function() {
-      return this.ytPlayer ? this.ytPlayer.getVolume() / 100.0 : 1;
+      return this.volumeInfo ? this.volumeInfo : this.ytPlayer.getVolume() / 100.0;
     },
 
     setVolume: function(percentAsDecimal) {
@@ -530,6 +530,7 @@ THE SOFTWARE. */
       }
 
       this.ytPlayer.setVolume(percentAsDecimal * 100.0);
+      this.volumeInfo = percentAsDecimal;
       this.setTimeout( function(){
         this.trigger('volumechange');
       }, 50);
